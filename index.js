28a79e5bb280be75ebdbe2391ea8fae8255e4809
AndrewSimons3 <simdrew07@gmail.com>
2021-08-09 20:48:08 -0500

    nls implementationusing async await

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 #!/usr/bin/env node
 
+
 const fs = require('fs');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
 
   if (err) {
-    throw new Error(err);
+    console.log(err);
   }
 
-  console.log(filenames);
+  for (let filename of filenames) {
+    try {
+    const stats = await lstat(filename);
+
+    console.log(filename, stats.isFile());
+    } catch (err) {
+      console.log(err);
+    }
+  }
 });
+
+//Method #2
+// const lstat = util.promisify(fs.lstat);
+
+//Method #3
+const { lstat } = fs.promises;
+
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+
+//       resolve(stats);
+//     })
+//   });
+// };
